99cde3a758eba5ae544b6a3ae5a200c6198a0f15
Anuja <anuja.ulp@gmail.com>
2019-10-25 23:21:25 +0800

    Update README.md

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+/**
+ * This is a function which returned masked value for given parameter.
+ *
+ * @param {string} input - Text needs to be masked
+ * @param {number} startIndex - Starting index of the text for masking
+ * @param {number} endIndex - Last index of the text for masking
+ * @param {string} mask - A symbol aces as the mask
+ * @return {string} masked text
+ *
+ */
 module.exports = (input, startIndex, endIndex, mask) => {
     for (let i = 0; i < input.length; i++) {
         if (i >= startIndex && i <= endIndex) {
